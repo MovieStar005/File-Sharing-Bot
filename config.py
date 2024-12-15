b324167968b3ea5c2d31eebf6b778f07bbeb1001
MovieStar005 <152777826+MovieStar005@users.noreply.github.com>
2024-12-15 13:32:43 +0530

    Update config.py

--- config.py
+++ config.py
@@ -30,7 +30,7 @@ from os import environ
 API = environ.get("API", "modijiurl.com") # shortlink api
 URL = environ.get("URL", "cc5016ec98fc49df57b0a9e710a3805cb7fb421a") # shortlink domain without https://
 VERIFY_TUTORIAL = environ.get("VERIFY_TUTORIAL", "https://t.me/All_Movie_Star_Link") # how to open link 
-BOT_USERNAME = environ.get("BOT_USERNAME", @XDIK_bot") # bot username without @
+BOT_USERNAME = environ.get("BOT_USERNAME", "XDIK_bot") # bot username without @
 VERIFY = environ.get("VERIFY", "True") # set True Or False and make sure spelling is correct and first letter capital.
 
 #Database 
